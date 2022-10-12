9e5600b9d3873d97442e9bd2cacdb2a35173a608
EkaterinaTadueva <etadueva@bk.ru>
2022-10-12 21:50:28 +0300

    Создала репозиторий

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+const slider = document.querySelector('.swiper');
+const sliderPagination = document.querySelector('.swiper__pagination');
+const sliderButtonNext = document.querySelector('.swiper-button-next');
+const sliderButtonPrev = document.querySelector('.swiper-button-prev');
+
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+const swiper = new Swiper(slider, {
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: sliderPagination,
+  },
+
+  navigation: {
+    nextEl: sliderButtonNext,
+    prevEl: sliderButtonPrev,
+  },
+});
